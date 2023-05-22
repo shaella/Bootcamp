ad3d3a46ed5b811e4983b32b24bdae196c4cebba
shaella <shirouchan38@hotmail.com>
2023-05-22 15:49:52 +0700

    day 1 week 3,

---
+++ exercise2/Program.cs
@@ -0,0 +1,42 @@
+﻿class Tree 
+{
+	public string? name;
+	public string? fruit;
+	public int? maxheight;
+	public int? durationofmaturing;
+	
+	public void Grow()
+	{
+		Console.WriteLine("Tree will grow");
+	}
+	public Tree(string a, int b, int c) 
+	{
+		fruit = a;
+		maxheight = b;
+		durationofmaturing = c;
+	}
+}
+ 
+class Programtorun
+{
+	static void Main() 
+	{
+		Tree mango = new Tree("has fruit", 3, 10);
+		mango.name = "Mango";
+		mango.Grow();
+		Console.WriteLine($"{mango.name} {mango.fruit} and the tree will grow {mango.maxheight} meters in {mango.durationofmaturing} years");
+		Tree apple = new Tree("has fruit", 3, 10);
+		apple.name = "Apple";
+		Console.WriteLine($"Another Tree is {apple.name}");
+		const string monthtoproducefruit = "January";
+		const int waitingtime = 7;
+		(string x, int y) = ProducingFruit(monthtoproducefruit, waitingtime);
+		Console.WriteLine($"The tree {apple.name} will produce fruits on {x} in {y} years ");
+	}
+	
+	static (string, int) ProducingFruit(string monthtoproducefruit, int waitingtime) 
+	{
+		return (monthtoproducefruit, waitingtime);
+	}
+}
+
