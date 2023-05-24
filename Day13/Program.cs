810ee9d932efe2e1817ce7e4d8c2889eee923f08
shaella <shirouchan38@hotmail.com>
2023-05-24 16:11:58 +0700

    Day 13

---
+++ Day13/Program.cs
@@ -0,0 +1,19 @@
+﻿// delegate
+
+public delegate void Seminar(string name, string when, int duration, int fee);
+
+class Program 
+{
+	static void Main() 
+	{
+		Seminar exampdelegate1 = new Seminar(ReceivSeminar);
+		exampdelegate1("Hari Bumi", "Wednesday, 8 AM", 2, 100000);
+		Console.ReadKey(); 
+	}
+	
+	public static void ReceivSeminar(string rName, string rWhen, int rDuration, int rFee) 
+	{
+		Console.WriteLine("Minggu ini akan diselenggarakan dua seminar:");
+		Console.WriteLine($"Seminar 1\n Judul: {rName}\n  Hari, pukul: {rWhen}\n   Durasi: {rDuration}\n    Biaya: {rFee}");
+	}
+}
--- exercise/Program.cs
+++ exercise/Program.cs
@@ -4,6 +4,54 @@
 //kode int b = int.Parse(Console.ReadLine());
 //kode Console.WriteLine(a+b);
 
-Console.ReadKey();
-Console.ReadKey();
-Console.ReadKey();
+//Console.ReadKey();
+//Console.ReadKey();
+//Console.ReadKey();
+
+public delegate void ResultCallback(int result);
+
+public class Number 
+{
+	public int number;
+	public ResultCallback resultCallback;
+	
+	public Number(int numberx, ResultCallback resultCallbackx) 
+	{
+		number = numberx;
+		resultCallback = resultCallbackx;
+	}
+	
+	public void Sum() 
+	{
+		int result = 0;
+		for (int i=1;i<=number;i++) 
+		{
+			result = result + i;
+		}
+		
+		if(resultCallback != null) 
+		{
+			resultCallback(result);
+		}
+	}
+}
+
+class MainProgram
+{
+	static void Main() 
+	{
+		ResultCallback myResultCallback = new ResultCallback(resultCallbackMethod);
+		int number = 10;
+		
+		Number obj = new Number(number, myResultCallback);
+		
+		Thread t1 = new Thread(new ThreadStart(obj.Sum));
+		t1.Start();
+		Console.Read();
+	}
+	
+	public static void resultCallbackMethod(int result) 
+	{
+		Console.WriteLine("The Result is "+ result);
+	}
+}
