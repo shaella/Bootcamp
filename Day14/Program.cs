8f19c2af64325ffcd51b74e9b7154efba800d665
shaella <shirouchan38@hotmail.com>
2023-05-25 16:20:34 +0700

    D14, 2nd commit

--- Day14/Program.cs
+++ Day14/Program.cs
@@ -1,13 +1,29 @@
-﻿class Program 
- {
- 	static void Main(string[] args) 
-	{
-		Console.WriteLine("Enter two numbers:");
-		Console.WriteLine("Number 1:");
-		int fourteenx = Convert.ToInt32(Console.ReadLine());
-		Console.WriteLine("Number 2:");
-		int fourteenx2 = Convert.ToInt32(Console.ReadLine());
-		int resultxx = fourteenx / fourteenx2;
-		Console.WriteLine(resultxx);
-	}
- }
+﻿namespace DayFourteen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers:");
+            Console.WriteLine("Number 1:");
+            int fourteenx = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number 2:");
+            int fourteenx2 = Convert.ToInt32(Console.ReadLine());
+            int resultxx = fourteenx / fourteenx2;
+ //           Console.WriteLine(resultxx);
+
+            try
+            {
+                int resultxx = Convert.ToInt32(Console.ReadLine()) / 0;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error: Cannot divide by zero!");
+            }
+            finally
+            {
+                Console.WriteLine("Cleaning up resources...");
+            }
+        }
+    }
+}
