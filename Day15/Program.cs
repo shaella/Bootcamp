067d1925d1cb5f8ba35b2298a54f742df2f3dac4
shaella <shirouchan38@hotmail.com>
2023-05-26 16:00:04 +0700

    2nd commit

--- Day15/Program.cs
+++ Day15/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Hari ke-9,
+//*String and Text Handling
+
+namespace hsembilan 
+{
+	class Program 
+	{
+		static void Main(string[] args) 
+		{
+			string title = "Hari ini";
+			int tanggal = 26;
+			Months bulan = Months.Mei;
+			Activities apygdilakukan = Activities.BelajarCSharp;
+			
+			string kalimat = Convert.ToString($"title punya {title.Length} karakter: {title[0]}, {title[1]}, {title[2]}, {title[3]}, {title[4]}, <spasi>, {title[6]}, {title[7]}, dibaca 'Hari ini'");
+			string KALIMAT = kalimat.ToUpper();
+			Console.WriteLine(KALIMAT);
+			string[] xkatax = kalimat.Split(' ');
+			foreach (char kata in kalimat) 
+			{
+				Console.WriteLine(kata + "#");
+			}
+		
+		}
+	}
+}
+
+public enum Months 
+{
+	Januari,
+	Februari,
+	Maret,
+	April,
+	Mei,
+	Juni,
+	Juli,
+	Agustus,
+	September,
+	Oktober,
+	November,
+	Desember
+}
+
+public enum Activities 
+{
+	BelajarCSharp,
+	BelajarGit,
+	UnggahHasilKaryaKeGitHub
+}
