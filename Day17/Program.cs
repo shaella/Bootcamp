c6054f04bbd13c2b310f9822861934d1db509a4e
shaella <shirouchan38@hotmail.com>
2023-06-09 13:11:28 +0700

    adding .gitignore

--- Day17/Program.cs
+++ Day17/Program.cs
@@ -1,62 +1,18 @@
-﻿using System;
-
-class MyClass : IDisposable {
-	private bool disposed = false;
-	
-	// Method untuk membebaskan sumber daya unmanaged
-	private void CleanupUnmanagedResources() {
-		Console.WriteLine("Cleaning up unmanaged resources...");
+﻿using System.Diagnostics;
+using System.IO;
+class Program 
+{
+	static void Main() 
+	{
+		int x = 10;
+		int y = 20;
+		
+		Trace.Listeners.Add(new ConsoleTraceListener());
+		Trace.WriteLine($"x = {0}, y = {1}", x, y);
+		TextWriterTraceListener listener = new TextWriterTraceListener("trace.log");
+		
+		Debug.Fail("This is a forced failure.");
+		Debug.Assert(x < y, "x should be less than y.");
+		Debug.Assert(x > y, "x should be greater than y.");
 	}
-	
-	// Implementasi metode Dispose
-	public void Dispose() {
-		Dispose(true);
-		GC.SuppressFinalize(this);
-	}
-	
-	protected virtual void Dispose(bool disposing) {
-		if (disposed) {
-			return;
-		}
-		
-		if (disposing) {
-			Console.WriteLine("Disposing managed resources...");
-			// Memanggil metode Dispose bagi objek yang memiliki referensi ke objek saat ini
-		}
-		
-		CleanupUnmanagedResources();
-		
-		disposed = true;
-	}
-	
-	~MyClass() {
-		Console.WriteLine("Finalizing object...");
-		Dispose(false);
-	}
-}
-
-class Program {
-	static void Main() {
-		// Menggunakan objek dengan using statement
-		using (var obj1 = new MyClass()) {
-			Console.WriteLine("Using object with 'using' statement...");
-		}
-		
-		// Menggunakan objek tanpa using statement
-		var obj2 = new MyClass();
-		Console.WriteLine("Using object without 'using' statement...");
-		obj2.Dispose();
-		
-		// Membuat objek dan menghapusnya secara eksplisit
-		var obj3 = new MyClass();
-		Console.WriteLine("Creating and disposing object explicitly...");
-		obj3.Dispose();
-		
-		// Membuat objek tanpa memanggil dispose atau garbage collection
-		var obj4 = new MyClass();
-		Console.WriteLine("Creating object without disposing or finalizing it...");
-		obj4 = null;
-
-        Console.ReadLine();
-    }
 }
