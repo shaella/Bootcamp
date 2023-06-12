51c98af4978c46996c7ea5056b6afba8e52e2aba
shaella <shirouchan38@hotmail.com>
2023-06-12 15:45:57 +0700

    Adding Task and Async-Await code samples

---
+++ Day19/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace NineteenTask
+{
+	class Program
+	{
+		static async Task Main()
+		{
+			Task task = new Task(TugasDua);
+			//Thread mt = new Thread(Tugas);
+			task.Start();
+			task.Wait();
+			await Tugas();
+		}
+		public static async Task Tugas()
+		{
+			string aha = "hello";
+			string bha = "everybody";
+			for (int i = 0; i < 20; i++)
+			{
+				await Task.Delay(TimeSpan.FromSeconds(10));
+				Console.WriteLine(aha + " " + bha);
+				Console.WriteLine("Printing just ended ...");
+			}
+		}
+
+		public static void TugasDua()
+		{
+			int x = 10;
+			int y = 20;
+			int z = x + y;
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(z + (x * y));
+			}
+		}
+	}
+}
+
+
--- exercise/Program.cs
+++ exercise/Program.cs
@@ -37,3 +37,49 @@
 //		Console.WriteLine(p1,p2,p3);
 //	}
 //}
+
+//using System;  
+//using System.Collections.Generic;  
+//using System.Linq;  
+//public static class demo  
+//{  
+//    public static void Main()  
+//    {  
+//        List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };  
+//        List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);  
+  
+//        foreach (var num in evenNumbers)  
+//        {  
+//            Console.Write("{0} ", num);  
+//       }  
+//        Console.WriteLine();  
+//        Console.Read();  
+  
+//    }  
+//}  
+
+using System;
+using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Main Method Started......");
+
+            SomeMethod();
+
+            Console.WriteLine("Main Method End");
+            Console.ReadLine();
+        }
+
+        public async static void SomeMethod()
+        {
+            Console.WriteLine("Some Method Started......");
+
+            //Thread.Sleep(TimeSpan.FromSeconds(10));
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Console.WriteLine("\\n");
+            Console.WriteLine("Some Method End");
+        }
+    }
