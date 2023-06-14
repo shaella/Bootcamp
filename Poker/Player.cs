22d5873b6ca47ae9569838a2199ec252ffe5ebba
shaella <shirouchan38@hotmail.com>
2023-06-14 17:03:02 +0700

    Update Poker

--- Poker/Player.cs
+++ Poker/Player.cs
@@ -1,43 +1,44 @@
 using IPlayerLib;
 namespace PlayerLib
-public class Player : IPlayer
 {
-	private int _ID;
-	private	string _name;
-	private int _chips;
-	public int ID
-	{
-		set
-		{
-			_ID = id;
-		}
-		get
-		{
-			return _ID;
-		}
-	}
-	public string name
-	{
-		set
-		{
-			_name = name;
-		}
-		get
-		{
-			return _name;
-		}
-	}
-	public int chips
-	{
-		set
-		{
-			_chips = chips;
-		}
-		get
-		{
-			return _chips;
-		}
-	}
-	
-	
+    public class Player : IPlayer
+    {
+        private int _ID;
+        private string _name;
+        private int _chips;
+        public int ID
+        {
+            set
+            {
+                _ID = id;
+            }
+            get
+            {
+                return _ID;
+            }
+        }
+        public string name
+        {
+            set
+            {
+                _name = name;
+            }
+            get
+            {
+                return _name;
+            }
+        }
+        public int chips
+        {
+            set
+            {
+                _chips = chips;
+            }
+            get
+            {
+                return _chips;
+            }
+        }
+    }
 }
+
